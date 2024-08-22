2d6c9ed926ebe57d26b52df805798533ad01369a
Reimnop <rayngu2005@gmail.com>
2024-08-22 19:13:00 +0700

    Make it trimmable

--- ParallelAnimationSystem/Program.cs
+++ ParallelAnimationSystem/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using CommandLine;
+using OpenTK.Graphics.OpenGL4;
 using ParallelAnimationSystem;
 
 public class Program
 {
     [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(Options))]
+    [DynamicDependency(DynamicallyAccessedMemberTypes.PublicMethods, typeof(GL))]
     public static async Task Main(string[] args)
     {
         await Parser.Default
