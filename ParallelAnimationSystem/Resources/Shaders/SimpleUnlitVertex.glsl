e8326012bb2899b0f2d48a531962676079f54872
Reimnop <rayngu2005@gmail.com>
2024-08-27 17:38:51 +0700

    Fix broken vertex shader

--- ParallelAnimationSystem/Resources/Shaders/SimpleUnlitVertex.glsl
+++ ParallelAnimationSystem/Resources/Shaders/SimpleUnlitVertex.glsl
@@ -11,7 +11,7 @@ void main() {
     // Since aPos is always in the range [-0.5, 0.5],
     // we can use it to directly calculate UVs,
     // without wasting space in the vertex buffer
-    vUv = aPos + 0.5;
+    vUv = aPos + vec2(0.5);
     
     gl_Position = vec4(vec2(uMvp * vec3(aPos, 1.0)), uZ, 1.0);
 }
