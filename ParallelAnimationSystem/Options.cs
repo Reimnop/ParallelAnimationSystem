789abc9364ce423ce86a5678513d897956f0aef7
Reimnop <rayngu2005@gmail.com>
2024-09-29 13:41:05 +0700

    Update args docs

--- ParallelAnimationSystem/Options.cs
+++ ParallelAnimationSystem/Options.cs
@@ -5,30 +5,30 @@ namespace ParallelAnimationSystem;
 
 public class Options
 {
-    [Option('l', "level", Required = true, HelpText = "Path to the level file (.lsb or .vgd)")]
+    [Option('l', "level", Required = true, HelpText = "Path to the level file (.lsb or .vgd).")]
     public required string LevelPath { get; set; }
     
-    [Option('a', "audio", Required = true, HelpText = "Path to the audio file")]
+    [Option('a', "audio", Required = true, HelpText = "Path to the audio file.")]
     public required string AudioPath { get; set; }
     
-    [Option("format", Required = false, HelpText = "The format of the level file (lsb/vgd)")]
+    [Option("format", Required = false, HelpText = "The format of the level file (lsb/vgd).")]
     public LevelFormat? Format { get; set; }
     
-    [Option("vsync", Required = false, HelpText = "Enable VSync")]
+    [Option("vsync", Required = false, HelpText = "Enable VSync.")]
     public bool VSync { get; set; }
     
-    [Option("workers", Required = false, Default = 4, HelpText = "Number of worker threads, set to -1 to use all available threads")]
+    [Option("workers", Required = false, Default = 4, HelpText = "Number of worker threads, set to -1 to use all available threads.")]
     public int WorkerCount { get; set; }
     
-    [Option("seed", Required = false, Default = -1, HelpText = "Seed for the random number generator, set to -1 to use a random seed")]
+    [Option("seed", Required = false, Default = -1, HelpText = "Seed for the random number generator, set to -1 to use a random seed.")]
     public long Seed { get; set; }
     
-    [Option("speed", Required = false, Default = 1.0f, HelpText = "Sets the playback speed")]
+    [Option("speed", Required = false, Default = 1.0f, HelpText = "Sets the playback speed.")]
     public float Speed { get; set; } = 1.0f;
     
-    [Option("backend", Required = false, Default = "opengl", HelpText = "The rendering backend to use (opengl/opengles)")]
+    [Option("backend", Required = false, Default = "opengl", HelpText = "Sets the rendering backend to use (opengl/opengles).")]
     public required string Backend { get; set; }
     
-    [Option("experimental-enable-text-rendering", Required = false, Default = false, HelpText = "Enable experimental text rendering")]
+    [Option("experimental-enable-text-rendering", Required = false, Default = false, HelpText = "Enable experimental text rendering.")]
     public bool EnableTextRendering { get; set; }
 }
