a25477e2e11e173532872bf1ffd90e43bbcebcd9
Reimnop <rayngu2005@gmail.com>
2026-01-12 21:24:43 +0700

    Remove DotSettings

--- ParallelAnimationSystem/Core/Data/ParentTemporalOffsets.cs
+++
@@ -1,3 +0,0 @@
-namespace ParallelAnimationSystem.Core.Data;
-
-public record struct ParentTemporalOffsets(float Position, float Scale, float Rotation);
--- ParallelAnimationSystem/Core/Data/ParentTypes.cs
+++
@@ -1,3 +0,0 @@
-namespace ParallelAnimationSystem.Core.Data;
-
-public record struct ParentTypes(bool Position, bool Scale, bool Rotation);
