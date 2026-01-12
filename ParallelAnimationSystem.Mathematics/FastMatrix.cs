e00fd616c9f8cadf04aef1a6832921ab71db8f7c
Reimnop <rayngu2005@gmail.com>
2026-01-12 21:56:28 +0700

    Micro optimize for no reason

---
+++ ParallelAnimationSystem.Mathematics/FastMatrix.cs
@@ -0,0 +1,72 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace ParallelAnimationSystem.Mathematics;
+
+public static class FastMatrix
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Matrix3x2 GetTranslationMatrix(float tx, float ty)
+        => new(
+            1f, 0f,
+            0f, 1f,
+            tx, ty);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Matrix3x2 GetScaleMatrix(float sx, float sy)
+        => new(
+            sx, 0f,
+            0f, sy,
+            0f, 0f);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Matrix3x2 GetRotationMatrix(float phi)
+    {
+        var (sin,cos) = MathF.SinCos(phi);
+        
+        return new Matrix3x2(
+            cos, sin,
+            -sin, cos,
+            0f, 0f);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Matrix3x2 GetTranslationScaleMatrix(float tx, float ty, float sx, float sy)
+        => new(
+            sx, 0f,
+            0f, sy,
+            tx, ty);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Matrix3x2 GetTranslationRotationMatrix(float tx, float ty, float phi)
+    {
+        var (sin,cos) = MathF.SinCos(phi);
+        
+        return new Matrix3x2(
+            cos, sin,
+            -sin, cos,
+            tx, ty);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Matrix3x2 GetRotationScaleMatrix(float sx, float sy, float phi)
+    {
+        var (sin,cos) = MathF.SinCos(phi);
+        
+        return new Matrix3x2(
+            cos * sx, sin * sx,
+            -sin * sy, cos * sy,
+            0f, 0f);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Matrix3x2 GetTranslationRotationScaleMatrix(float tx, float ty, float sx, float sy, float phi)
+    {
+        var (sin,cos) = MathF.SinCos(phi);
+        
+        return new Matrix3x2(
+            cos * sx, sin * sx,
+            -sin * sy, cos * sy,
+            tx, ty);
+    }
+}
--- ParallelAnimationSystem/Core/BeatmapObjectsProcessor.cs
+++ ParallelAnimationSystem/Core/BeatmapObjectsProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Numerics;
 using Pamx.Common.Enum;
 using ParallelAnimationSystem.Core.Beatmap;
+using ParallelAnimationSystem.Mathematics;
 using ParallelAnimationSystem.Util;
 
 namespace ParallelAnimationSystem.Core;
@@ -44,23 +45,59 @@ public class BeatmapObjectsProcessor(PerFrameDataCache cache, Timeline timeline)
         while (true)
         {
             parentDepth++;
-            
-            if (parentType.HasFlag(ParentType.Scale))
-            {
-                var scale = beatmapObject.ScaleSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Scale, context);
-                matrix *= Matrix3x2.CreateScale(scale);
-            }
 
-            if (parentType.HasFlag(ParentType.Rotation))
+            switch (parentType)
             {
-                var rotation = beatmapObject.RotationSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Rotation, context);
-                matrix *= Matrix3x2.CreateRotation(rotation);
-            }
-
-            if (parentType.HasFlag(ParentType.Position))
-            {
-                var position = beatmapObject.PositionSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Position, context);
-                matrix *= Matrix3x2.CreateTranslation(position);
+                case ParentType.Position:
+                {
+                    var position = beatmapObject.PositionSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Position, context);
+                    matrix *= FastMatrix.GetTranslationMatrix(position.X, position.Y);
+                    break;
+                }
+                case ParentType.Scale:
+                {
+                    var scale = beatmapObject.ScaleSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Scale, context);
+                    matrix *= FastMatrix.GetScaleMatrix(scale.X, scale.Y);
+                    break;
+                }
+                case ParentType.Rotation:
+                {
+                    var rotation = beatmapObject.RotationSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Rotation, context);
+                    matrix *= FastMatrix.GetRotationMatrix(rotation);
+                    break;
+                }
+                case ParentType.Position | ParentType.Rotation:
+                {
+                    var position = beatmapObject.PositionSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Position, context);
+                    var rotation = beatmapObject.RotationSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Rotation, context);
+                    matrix *= FastMatrix.GetTranslationRotationMatrix(position.X, position.Y, rotation);
+                    break;
+                }
+                case ParentType.Position | ParentType.Scale:
+                {
+                    var position = beatmapObject.PositionSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Position, context);
+                    var scale = beatmapObject.ScaleSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Scale, context);
+                    matrix *= FastMatrix.GetTranslationScaleMatrix(position.X, position.Y, scale.X, scale.Y);
+                    break;
+                }
+                case ParentType.Rotation | ParentType.Scale:
+                {
+                    var rotation = beatmapObject.RotationSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Rotation, context);
+                    var scale = beatmapObject.ScaleSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Scale, context);
+                    matrix *= FastMatrix.GetRotationScaleMatrix(scale.X, scale.Y, rotation);
+                    break;
+                }
+                case ParentType.All:
+                {
+                    var position = beatmapObject.PositionSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Position, context);
+                    var rotation = beatmapObject.RotationSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Rotation, context);
+                    var scale = beatmapObject.ScaleSequence.Interpolate(time - beatmapObject.StartTime - parentOffset.Scale, context);
+                    matrix *= FastMatrix.GetTranslationRotationScaleMatrix(
+                        position.X, position.Y,
+                        scale.X, scale.Y,
+                        rotation);
+                    break;
+                }
             }
 
             if (!timeline.BeatmapObjects.TryGetParent(beatmapObject.Id.Numeric, out var parent) || parent is null) 
