a333e7efec74855e730626fef350bddb7ce7021a
Reimnop <rayngu2005@gmail.com>
2026-03-01 13:28:21 +0700

    Get rid of ffmpeg frontend

--- ParallelAnimationSystem.FFmpeg/FrameData.cs
+++
@@ -1,8 +0,0 @@
-namespace ParallelAnimationSystem.FFmpeg;
-
-public class FrameData(int width, int height)
-{
-    public int Width { get; } = width;
-    public int Height { get; } = height;
-    public byte[] Data { get; } = new byte[width * height * 4];
-}
