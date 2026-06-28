7b3916b52687560fff1066b78ed3b29b07013b06
Reimnop <rayngu2005@gmail.com>
2026-06-28 15:45:15 +0700

    Add Play/Pause on space

--- ParallelAnimationSystem.Avalonia/Views/MainWindow.axaml.cs
+++ ParallelAnimationSystem.Avalonia/Views/MainWindow.axaml.cs
@@ -1,3 +1,6 @@
+using Avalonia.Input;
+using ParallelAnimationSystem.Avalonia.ViewModels;
+
 namespace ParallelAnimationSystem.Avalonia.Views;
 
 public partial class MainWindow : ShadUI.Window
--- ParallelAnimationSystem.Avalonia/Views/PlaybackView.axaml.cs
+++ ParallelAnimationSystem.Avalonia/Views/PlaybackView.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
 using Avalonia.Input;
 using Lucide.Avalonia;
 using ParallelAnimationSystem.Avalonia.Controls;
@@ -9,6 +10,8 @@ namespace ParallelAnimationSystem.Avalonia.Views;
 public partial class PlaybackView : UserControl
 {
     private readonly PlayPauseAnimation playPauseAnim;
+
+    private PlaybackViewModel? currentViewModel;
     
     public PlaybackView()
     {
@@ -16,28 +19,37 @@ public partial class PlaybackView : UserControl
         
         playPauseAnim = new PlayPauseAnimation(PlayPauseIconContainer);
     }
-    
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        
+        currentViewModel?.PlayPauseIconDisplayRequested -= OnPlayPauseIconDisplayRequested;
+        
+        currentViewModel = DataContext as PlaybackViewModel;
+        currentViewModel?.PlayPauseIconDisplayRequested += OnPlayPauseIconDisplayRequested;
+    }
+
+    private void OnPlayPauseIconDisplayRequested(object? sender, LucideIconKind e)
+    {
+        PlayPauseIcon.Kind = e;
+        _ = playPauseAnim.Trigger();
+    }
+
     private void TimelineScrubber_OnSeekRequested(object? sender, SeekRequestedEventArgs e)
     {
-        if (DataContext is not PlaybackViewModel vm)
-            return;
-
-        vm.Seek(e.Position);
+        currentViewModel?.Seek(e.Position);
     }
 
     private void PlaybackRootPanel_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
     {
-        if (DataContext is not PlaybackViewModel vm)
+        if (currentViewModel is null)
             return;
 
         if (e.InitialPressMouseButton == MouseButton.Left)
         {
             e.Handled = true;
-            
-            PlayPauseIcon.Kind = vm.IsPlaying ? LucideIconKind.Pause : LucideIconKind.Play;
-            _ = playPauseAnim.Trigger();
-            
-            vm.PlayPause();
+            currentViewModel.PlayPauseDisplayIcon();
         }
     }
 
@@ -51,5 +63,17 @@ public partial class PlaybackView : UserControl
     {
         if (e.InitialPressMouseButton == MouseButton.Left)
             e.Handled = true;
+    }
+
+    private void PlaybackRootPanel_OnKeyUp(object? sender, KeyEventArgs e)
+    {
+        if (currentViewModel is null)
+            return;
+
+        if (e.Key == Key.Space)
+        {
+            e.Handled = true;
+            currentViewModel.PlayPauseDisplayIcon();
+        }
     }
 }
